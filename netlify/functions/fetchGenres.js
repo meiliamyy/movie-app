81bf39b4a9e64d1dd25c74fe51fe2a70acf044ea
meiliamyy <meiliarahmi8@gmail.com>
2025-01-15 23:40:37 +0700

    fix node-fetch module at function

--- netlify/functions/fetchGenres.js
+++ netlify/functions/fetchGenres.js
@@ -1,12 +1,12 @@
-const fetch = require("node-fetch");
-
 exports.handler = async (event, context) => {
+  const fetch = (await import("node-fetch")).default;
   const API_KEY = process.env.TMDB_API_KEY;
   const BASE_URL = "https://api.themoviedb.org/3/genre/movie/list";
 
   try {
     const response = await fetch(`${BASE_URL}?api_key=${API_KEY}`);
     const data = await response.json();
+    console.log(data);
 
     return {
       statusCode: 200,
--- netlify/functions/fetchSearchMovies.js
+++ netlify/functions/fetchSearchMovies.js
@@ -1,6 +1,5 @@
-const fetch = require("node-fetch");
-
 exports.handler = async (event, context) => {
+  const fetch = (await import("node-fetch")).default;
   const API_KEY = process.env.TMDB_API_KEY;
   const BASE_URL = "https://api.themoviedb.org/3/search/movie";
 
